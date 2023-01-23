70f3c9f686fe3fbdbcc0e217ca5f159152f3945f
Theuz <matheusqueiroz2002@hotmail.com>
2023-01-23 13:11:20 -0300

    Uptades

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('header button')
 
-button.addEventListener('click', add)
+button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
     
-    const today = new Date().toLocaleDateString("pt-br").slice(0,-5) 
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
     const dayExists = nlwSetup.dayExists(today)
 
     if (dayExists) {
-        alert("Dia já incluso")
+        alert("Dia já incluso! 👍")
         return
     }
 
-    alert('adicionado com sucesso')
     nlwSetup.addDay(today)
 }
 
 function save() {
-    localStorage.setItem('NLWSetup@habits', 
-    JSON.stringify(nlwSetup.data))
-} 
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
 
-const data = JSON.parse(localStorage.getItem("NLWSetup@habits"))
-nlwSetup.setData(data) 
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
 nlwSetup.load()
 
 
