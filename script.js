0764807f41c1eb242c50893c182e1553915823a7
Theuz <123218001+Theuzqrz@users.noreply.github.com>
2023-01-25 18:20:36 -0300

    Update

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
+
+
 button.addEventListener("click", add)
 form.addEventListener("change", save)
+
+
 
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
+
+
 
   if (dayExists) {
     alert("Dia já incluso!👍")
@@ -17,9 +23,13 @@ function add() {
   nlwSetup.addDay(today)
 }
 
+
+
 function save() {
   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
+
+
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
